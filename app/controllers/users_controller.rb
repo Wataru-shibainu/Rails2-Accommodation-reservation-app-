75875408134d20f60a6d205d30273442e9ef3a15
Wataru Mori <watarumori@WatarunoMacBook-Air.local>
2023-02-27 20:28:22 +0900

    pagesコントローラの名称をroomsに変更、仮の新規登録画面作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
   def index
   end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :email, :input_password, :confirmation_password))
+    if @user.save
+     
+      redirect_to :users
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
