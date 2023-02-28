6475acafdbe792a2d8d5889acbfac641d401dbae
Wataru Mori <watarumori@WatarunoMacBook-Air.local>
2023-02-28 22:44:25 +0900

    ログイン前、ログイン後別の画面遷移　仮実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,4 +3,7 @@ class RoomsController < ApplicationController
   def home
   end
   
+  def after_login
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
  
-  devise_for :users
+  devise_for :users, :controllers => {
+#    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+#    :sessions => 'users/sessions',
+#    :passwords => 'users/passwords'
+   }
+  
   root 'rooms#home'
   get 'users/index'
   get 'rooms/home'
+  get 'rooms/after_login'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   resources :users
