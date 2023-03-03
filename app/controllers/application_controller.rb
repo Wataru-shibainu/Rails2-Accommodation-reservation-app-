0ffaa0a5b21a2e6b64815ea12f4298df9623820b
Wataru Mori <watarumori@WatarunoMacBook-Air.local>
2023-03-03 20:30:17 +0900

    ログイン前、ログイン後の画面遷移実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(resource)
-    rooms_after_login_path
+    root_path
   end
   
 #  def after_sign_out_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
+#  attribute :image, :string, default: 'default-avatar.png'
+  
   mount_uploader :image, ImageUploader
   
 end
