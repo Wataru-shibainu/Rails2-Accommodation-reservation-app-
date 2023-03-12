435743b630a48c69d51b3390a3ead6da4c3b5904
Wataru Mori <watarumori@WatarunoMacBook-Air.local>
2023-03-12 16:55:41 +0900

    　チェックイン

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,18 +9,23 @@ class ReservationsController < ApplicationController
   def confirm
     @reservation = Reservation.new(params.require(:reservation).permit(:check_in_date, :check_out_date, :created_at,
                                                                        :user_id, :room_id, :reservation_number_of_people))
-    flash[:check_in_date] = nil
-    flash[:check_out_date] = nil
-    flash[:reservation_number_of_people] = nil
+    flash[:check_in_error] = nil
+    flash[:check_out_error] = nil
+    flash[:reservation_number_error] = nil
 
-    # 予約時入力必須項目(チェックイン日、チェックアウト日、人数)が未入力の場合、実行
-    unless @reservation.check_in_date.blank? || @reservation.check_out_date.blank? || @reservation.reservation_number_of_people.blank?
+    # 予約時入力必須項目(チェックイン日、チェックアウト日、人数)が未入力、チェックイン>チェックアウトの場合、実行
+    unless @reservation.check_in_date.blank? || @reservation.check_out_date.blank? ||
+           @reservation.reservation_number_of_people.blank? || @reservation.check_in_date > @reservation.check_out_date
       return
     end
 
-    flash[:check_in_date] = 'チェックイン日を入力してください' if @reservation.check_in_date.blank?
-    flash[:check_out_date] = 'チェックアウト日を入力してください' if @reservation.check_out_date.blank?
-    flash[:reservation_number_of_people] = '人数を入力してください' if @reservation.reservation_number_of_people.blank?
+    flash[:check_in_error] = 'チェックイン日を入力してください' if @reservation.check_in_date.blank?
+    flash[:check_out_error] = 'チェックアウト日を入力してください' if @reservation.check_out_date.blank?
+    if @reservation.check_in_date > @reservation.check_out_date
+      flash[:check_out_past] =
+        'チェックアウト日はチェックイン日よりも後の日付を選択してください'
+    end
+    flash[:reservation_number_error] = '人数を入力してください' if @reservation.reservation_number_of_people.blank?
     @room = Room.find_by(id: @reservation.room_id)
     render 'rooms/show'
   end
